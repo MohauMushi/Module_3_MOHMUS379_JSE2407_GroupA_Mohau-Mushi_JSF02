96e6f55deb2360bc8749e49e8ceeed299362bdcf
Mohau Mushi <david1mushi1@gmail.com>
2024-07-25 21:35:23 +0200

    updated ProductStore.js to remove set method

--- src/Stores/ProductStore.js
+++ src/Stores/ProductStore.js
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 
 function createProductStore() {
-  const { subscribe, set, update } = writable({
+  const { subscribe, update } = writable({
     products: [],
     categories: [],
     loading: false,
