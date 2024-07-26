ef07bac2e6e90cab5817f39a6c0198a5c52974e1
Mohau Mushi <david1mushi1@gmail.com>
2024-07-26 11:23:29 +0200

    Added Route component and Home page imports

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { Router } from "svelte-routing";
+  import { Router, Route } from "svelte-routing";
+  import Home from "./pages/Home.svelte";
   import Header from "./components/Header.svelte";
 
   export let url = "";
@@ -8,5 +9,8 @@
 <Router {url}>
   <div class="min-h-screen bg-gray-100">
     <Header />
+    <main class="container mx-auto px-4 py-8">
+      <Route path="/" component={Home} />
+    </main>
   </div>
 </Router>
