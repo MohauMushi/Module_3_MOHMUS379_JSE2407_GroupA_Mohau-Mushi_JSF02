f8bc72aff7f79ba826639e5100d9cdd7e6ca6812
Mohau Mushi <david1mushi1@gmail.com>
2024-07-26 10:23:24 +0200

    Created ProductGrid component to display multiple products, from the ProductCard Component

---
+++ src/components/ProductGrid.svelte
@@ -0,0 +1,13 @@
+<script>
+  import ProductCard from "./ProductCard.svelte";
+
+  export let products = [];
+</script>
+
+<div
+  class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
+>
+  {#each products as product (product.id)}
+    <ProductCard {product} />
+  {/each}
+</div>
