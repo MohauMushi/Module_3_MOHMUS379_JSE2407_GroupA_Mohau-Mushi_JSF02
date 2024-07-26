82597eb56ecd81c0bc63c0a064352fed5dfbeec6
Mohau Mushi <david1mushi1@gmail.com>
2024-07-26 10:31:16 +0200

    Implemented a product fetching and display using Svelte store

---
+++ src/pages/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+  import { productsStore, fetchProducts } from "../stores/productStore";
+  import ProductGrid from "../components/ProductGrid.svelte";
+
+  let products = [];
+
+  onMount(async () => {
+    await fetchProducts();
+  });
+
+  $: {
+    products = $productsStore.products;
+  }
+</script>
+
+<div class="space-y-8">
+  <ProductGrid {products} />
+</div>
