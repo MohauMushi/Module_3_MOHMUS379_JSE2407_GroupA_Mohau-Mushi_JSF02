6cafdff3fdf9d6ee14a41eb49ae72040eb24a0df
Mohau Mushi <david1mushi1@gmail.com>
2024-07-26 14:34:29 +0200

    Updated ProductCard component with new category display and added buttons for wishlist and add to cart

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -17,8 +17,40 @@
       <h2 class="text-lg font-semibold text-gray-900 mb-2 line-clamp-2">
         {product.title}
       </h2>
-      <p class="text-gray-600 mb-2 text-sm">{product.category}</p>
       <p class="text-indigo-600 font-bold">${product.price.toFixed(2)}</p>
+	  <div
+	  class="flex flex-col sm:flex-row justify-between items-start sm:items-center mt-2 space-y-2 sm:space-y-0"
+	>
+	  <span
+		class="px-2 py-1 bg-indigo-100 text-[#415a77] rounded-md text-xs font-medium"
+	  >{product.category}</span>
+	  <div class="flex items-center space-x-2">
+		<button
+		  class="p-1 rounded-full transition-colors duration-300"
+		>
+		  <svg
+			class="h-6 w-6 hover:text-red-500"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		  >
+			<path
+			  stroke-linecap="round"
+			  stroke-linejoin="round"
+			  stroke-width="2"
+			  d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
+			/>
+		  </svg>
+		</button>
+		<button
+		  class="inline-flex items-center justify-center px-3 py-2 bg-[#354961] text-white text-sm font-medium rounded-md hover:bg-[#415a77] transition-colors duration-300"
+		>
+		  Add To Cart
+		</button>
+	  </div>
+	</div>
     </div>
   </div>
 </Link>
