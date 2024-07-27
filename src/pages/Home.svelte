2d53eec01d89d02d6d2c5ae95708d7b511b26987
Mohau Mushi <david1mushi1@gmail.com>
2024-07-27 19:04:20 +0200

    Added loading state and spinner to Home page

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,18 +2,30 @@
   import { onMount } from "svelte";
   import { productsStore, fetchProducts } from "../stores/productStore";
   import ProductGrid from "../components/ProductGrid.svelte";
+  import LoadingSpinner from "../components/LoadingSpinner.svelte";
 
   let products = [];
+  let loading = true;
 
   onMount(async () => {
     await fetchProducts();
+
+    setTimeout(() => {
+      loading = false;
+    }, 1200);
   });
 
-  $: {
-    products = $productsStore.products;
-  }
+  $: products = $productsStore.products;
 </script>
 
 <div class="space-y-8">
-  <ProductGrid {products} />
+  {#if loading}
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+    >
+      <LoadingSpinner count={8} />
+    </div>
+  {:else}
+    <ProductGrid {products} />
+  {/if}
 </div>
