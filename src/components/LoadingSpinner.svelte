0b0450be502cb8cb19d9fec6c48e6086d86c7702
Mohau Mushi <david1mushi1@gmail.com>
2024-07-27 19:02:37 +0200

    Added a skeleton loader component with customizable count

---
+++ src/components/LoadingSpinner.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let count = 8;
+  </script>
+  
+  {#each Array(count) as _}
+	<div class="animate-pulse bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden">
+	  <div class="h-48 bg-gray-400"></div>
+	  <div class="p-4">
+		<div class="h-4 bg-gray-400 rounded w-3/4 mb-2"></div>
+		<div class="h-4 bg-gray-400 rounded w-1/2 mb-2"></div>
+		<div class="h-4 bg-gray-400 rounded w-1/4"></div>
+	  </div>
+	  <div class="p-4 flex justify-between items-center">
+		<div class="h-6 bg-gray-400 rounded w-1/4"></div>
+		<div class="h-8 bg-gray-400 rounded w-1/3"></div>
+	  </div>
+	</div>
+  {/each}
