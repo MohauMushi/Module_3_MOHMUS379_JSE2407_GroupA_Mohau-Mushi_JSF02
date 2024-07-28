f4a52352b513e32249d3c0b7c4a4d5c65b01763e
Mohau Mushi <david1mushi1@gmail.com>
2024-07-28 21:05:27 +0200

    Added a Sync to selectedCategory, sortOrder with filterSortStore, which synchronizes the components local state

--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -1,21 +1,24 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
-  
+	import { filterSortStore } from "../stores/filterSortStore";
+
 	export let categories = [];
 	export let selectedCategory = '';
-  
 	const dispatch = createEventDispatcher();
-  
 	function handleCategoryChange(event) {
 	  dispatch('filterChange', { category: event.target.value });
 	}
+  
+	filterSortStore.subscribe(value => {
+	  selectedCategory = value.selectedCategory;
+	});
   </script>
   
   <div class="w-full md:w-auto mb-4 md:mb-0">
 	<label for="category" class="block text-sm font-medium text-gray-700 mb-1">Filter by Category:</label>
 	<select
 	  id="category"
-	  value={selectedCategory}
+	  bind:value={selectedCategory}
 	  on:change={handleCategoryChange}
 	  class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
 	>
--- src/components/Sort.svelte
+++ src/components/Sort.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
-  
+	import { filterSortStore } from "../stores/filterSortStore";
 	export let sortOrder = '';
   
 	const dispatch = createEventDispatcher();
@@ -8,13 +8,16 @@
 	function handleSortChange(event) {
 	  dispatch('sortChange', { sortOrder: event.target.value });
 	}
+	filterSortStore.subscribe(value => {
+	  sortOrder = value.sortOrder;
+	});
   </script>
   
   <div class="ww-full md:w-auto">
 	<label for="sort" class="block text-sm font-medium text-gray-700 mb-1">Sort by Price:</label>
 	<select
 	  id="sort"
-	  value={sortOrder}
+	  bind:value={sortOrder}
 	  on:change={handleSortChange}
 	  class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
 	>
