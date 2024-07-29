cdede3f33f14c533c21aa9c8c3f584cfec0d8b5b
Mohau Mushi <david1mushi1@gmail.com>
2024-07-29 23:20:23 +0200

    Removed url export and update Router component

--- src/App.svelte
+++ src/App.svelte
@@ -2,17 +2,15 @@
   import { Router, Route } from "svelte-routing";
   import Home from "./pages/Home.svelte";
   import Header from "./components/Header.svelte";
-  import ProductDetail from "./pages/ProductDetail.svelte"
-
-  export let url = "";
+  import ProductDetail from "./pages/ProductDetail.svelte";
 </script>
 
-<Router {url}>
+<Router>
   <div class="min-h-screen bg-gray-100">
     <Header />
     <main class="container mx-auto px-4 py-8">
       <Route path="/" component={Home} />
-      <Route path="/product/:id" component={ProductDetail}/>
+      <Route path="/product/:id" component={ProductDetail} />
     </main>
   </div>
 </Router>
